96e432adac64d9437950e2ce8043ef80426a95dc
M Aslam <aslam4webz@gmail.com>
2025-07-15 05:13:02 +0100

    fix(users): resolve add user functionality and improve form handling
    - Fixed Add User button not opening the dialog
    - Added dedicated openAddUserDialog function for better state management
    - Ensured form is properly reset before showing the add user dialog
    - Improved dialog state management and form submission flow
    - Added proper TypeScript types and error handling
    - Fixed form validation and field requirements

--- src/components/NavMain.vue
+++ src/components/NavMain.vue
@@ -35,42 +35,41 @@ defineProps<{
   <SidebarGroup>
     <SidebarGroupLabel>Platform</SidebarGroupLabel>
     <SidebarMenu>
-      <Collapsible
-        v-for="item in items"
-        :key="item.title"
-        as-child
-        :default-open="item.isActive"
-        class="group/collapsible"
-      >
-        <SidebarMenuItem>
-          <CollapsibleTrigger as-child>
-            <SidebarMenuButton :tooltip="item.title" :as-child="!item.items?.length">
-              <template v-if="!item.items?.length">
-                <RouterLink :to="item.url" class="flex items-center w-full">
-                  <component :is="item.icon" v-if="item.icon" />
-                  <span>{{ item.title }}</span>
-                </RouterLink>
-              </template>
-              <template v-else>
+      <template v-for="item in items" :key="item.title">
+        <!-- Regular menu items without sub-items -->
+        <SidebarMenuItem v-if="!item.items?.length">
+          <SidebarMenuButton :tooltip="item.title" as-child>
+            <RouterLink :to="item.url" class="flex items-center w-full">
+              <component :is="item.icon" v-if="item.icon" />
+              <span>{{ item.title }}</span>
+            </RouterLink>
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+
+        <!-- Collapsible menu items with sub-items -->
+        <Collapsible v-else as-child :default-open="item.isActive" class="group/collapsible">
+          <SidebarMenuItem>
+            <CollapsibleTrigger as-child>
+              <SidebarMenuButton :tooltip="item.title">
                 <component :is="item.icon" v-if="item.icon" />
                 <span>{{ item.title }}</span>
                 <ChevronRight class="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-              </template>
-            </SidebarMenuButton>
-          </CollapsibleTrigger>
-          <CollapsibleContent>
-            <SidebarMenuSub>
-              <SidebarMenuSubItem v-for="subItem in item.items" :key="subItem.title">
-                <SidebarMenuSubButton as-child>
-                  <RouterLink :to="subItem.url" class="w-full block">
-                    <span>{{ subItem.title }}</span>
-                  </RouterLink>
-                </SidebarMenuSubButton>
-              </SidebarMenuSubItem>
-            </SidebarMenuSub>
-          </CollapsibleContent>
-        </SidebarMenuItem>
-      </Collapsible>
+              </SidebarMenuButton>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <SidebarMenuSub>
+                <SidebarMenuSubItem v-for="subItem in item.items" :key="subItem.title">
+                  <SidebarMenuSubButton as-child>
+                    <RouterLink :to="subItem.url" class="w-full block">
+                      <span>{{ subItem.title }}</span>
+                    </RouterLink>
+                  </SidebarMenuSubButton>
+                </SidebarMenuSubItem>
+              </SidebarMenuSub>
+            </CollapsibleContent>
+          </SidebarMenuItem>
+        </Collapsible>
+      </template>
     </SidebarMenu>
   </SidebarGroup>
 </template>
